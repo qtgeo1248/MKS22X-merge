8337328c34e3128b686cd0a8df88cb542ea121fa
qtgeo1248 <gzhou00@stuy.edu>
2019-03-15 19:52:28 -0400

    finished merge.java

--- Merge.java
+++ Merge.java
@@ -9,16 +9,16 @@ public class Merge {
         if (data.length <= 1) {
             return;
         }
-        System.out.println(Arrays.toString(data));
+        //System.out.println(Arrays.toString(data));
         int[] left = new int[(start + end) / 2 - start + 1];
         for (int i = 0; i < left.length; i++) {
             left[i] = data[i];
         }
-        System.out.println(Arrays.toString(left));
+        //System.out.println(Arrays.toString(left));
         mergesort(left, 0, left.length - 1);
         int[] right = new int[end - (start + end) / 2];
         for (int i = 0; i < right.length; i++) {
-            right[i] = data[i + (start + end) / 2];
+            right[i] = data[i + (start + end) / 2 + 1];
         }
         mergesort(right, 0, right.length - 1);
         int i = 0; //records for left array
@@ -26,17 +26,22 @@ public class Merge {
         for (int k = 0; k < data.length; k++) {
             if (i >= left.length) {
                 data[k] = right[j];
+                j++;
             } else if (j >= right.length) {
                 data[k] = left[i];
+                i++;
             } else {
                 int l = left[i];
                 int r = right[j];
                 if (l < r) {
                     data[k] = l;
+                    i++;
                 } else {
                     data[k] = r;
+                    j++;
                 }
             }
         }
+        //System.out.println(Arrays.toString(data));
     }
 }
