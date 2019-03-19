e33a2e2f432f69c6eed9c156495d6d4d67e25111
qtgeo1248 <gzhou00@stuy.edu>
2019-03-19 18:58:43 -0400

    added comments

--- Merge.java
+++ Merge.java
@@ -16,7 +16,7 @@ public class Merge {
 
     public static void mergesort(int[] data) {
         int[] temp = new int[data.length];
-        temp = Arrays.copyOf(data, data.length);
+        temp = Arrays.copyOf(data, data.length); //creation of my temp array
         mergesort(data, temp, 0, data.length - 1);
     }
 
@@ -36,14 +36,14 @@ public class Merge {
         moreSpace(right, 0, right.length - 1);
         int i = 0; //records for left array
         int j = 0; //records for right array
-        for (int k = 0; k < data.length; k++) {
+        for (int k = 0; k < data.length; k++) { //if you reached the end of the left side, you add the right
             if (i >= left.length) {
                 data[k] = right[j];
                 j++;
-            } else if (j >= right.length) {
+            } else if (j >= right.length) { //if you reached the end of the right side, you add the left
                 data[k] = left[i];
                 i++;
-            } else {
+            } else { //else, you compare the two values, and add the smallest one
                 int l = left[i];
                 int r = right[j];
                 if (l < r) {
@@ -58,22 +58,22 @@ public class Merge {
     }
 
     public static void mergesort(int[] data, int[] temp, int start, int end) {
-        if (end - start <= 128) {
+        if (end - start <= 128) { //insertion sort optimization
             insertionSort(data, start, end);
             return;
         }
         int i = start; //left part
         int j = (start + end) / 2 + 1; //right part
         mergesort(temp, data, i, j - 1);
-        mergesort(temp, data, j, end);
+        mergesort(temp, data, j, end); //switches back and forth to creat partitions
         for (int k = start; k <= end; k++) {
-            if (i > (start + end) / 2) {
+            if (i > (start + end) / 2) { //if you reached the end of the left side, you add the right
                 data[k] = temp[j];
                 j++;
-            } else if (j > end) {
+            } else if (j > end) { //if you reached the end of the right side, you add the left
                 data[k] = temp[i];
                 i++;
-            } else {
+            } else { //else, you compare the two values, and add the smallest one
                 int l = temp[i];
                 int r = temp[j];
                 if (l < r) {
